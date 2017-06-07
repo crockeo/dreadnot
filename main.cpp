7bad9b6bac4e6e2f339afe3c7b873b91570cfbe4
Cerek Hillen <cerekh@gmail.com>
2017-06-07 15:21:02 +0000

    Added a preprocessor directive to always include persistent process mode where possible.

--- main.cpp
+++ main.cpp
@@ -13,25 +13,32 @@ using namespace HL;
 
 int main()
 {
-    parse::trace_t trace;
-    try
+#ifdef __AFL_LOOP
+    while (__AFL_LOOP(1000))
     {
-        parse::trace_t trace = parse::lex(cin);
-    } catch (runtime_error& e)
-    {
-        cout << "Lex error: " << e.what() << endl;
-        return 1;
-    }
+#endif
+        parse::trace_t trace;
+        try
+        {
+            parse::trace_t trace = parse::lex(cin);
+        } catch (runtime_error& e)
+        {
+            cout << "Lex error: " << e.what() << endl;
+            return 1;
+        }
 
-    BrokenHeap<MallocHeap> brokenHeap;
-    try
-    {
-        parse::execute<BrokenHeap<MallocHeap> >(brokenHeap, trace);
-    } catch (runtime_error& e)
-    {
-        cout << "Execute error: " << e.what() << endl;
-    }
+        BrokenHeap<MallocHeap> brokenHeap;
+        try
+        {
+            parse::execute<BrokenHeap<MallocHeap> >(brokenHeap, trace);
+        } catch (runtime_error& e)
+        {
+            cout << "Execute error: " << e.what() << endl;
+        }
 
-    for (auto it = trace.begin(); it != trace.end(); it++)
-        free(*it);
+        for (auto it = trace.begin(); it != trace.end(); it++)
+            free(*it);
+#ifdef __AFL_LOOP
+    }
+#endif
 }
