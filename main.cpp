8c3356ede77c1999a50c6b67c5e8b0017b874a01
Cerek Hillen <cerekh@gmail.com>
2017-06-16 23:06:08 +0000

    Added better mode support.

--- main.cpp
+++ main.cpp
@@ -14,14 +14,28 @@ using namespace HL;
 
 char program_input[4096];
 
+void print_correct_usage()
+{
+    cout << "Correct usage:" << endl
+         << "  dreadnot [fuzzmode]" << endl
+         << "    dreadnot - Reads input from stdin, parses out input into alloc\
+ and free calls to test a heap." << endl
+         << "    fuzzmode - Used for FuzzBALL; provide a symblic block to the\
+ location of program_input in main.cpp." << endl;
+}
+
 int main(int argc, char **argv)
 {
     parse::trace_t trace;
-    if (argc == 2 && strcmp(argv[1], "fuzzmode") == 0)
+    if (argc == 2)
     {
-        istringstream input_stream(program_input);
-        if (!parse::lex(trace, input_stream))
-            return 1;
+        if (strcmp(argv[1], "fuzzmode") == 0)
+        {
+            istringstream input_stream(program_input);
+            if (!parse::lex(trace, input_stream))
+                return 1;
+        } else
+            print_correct_usage();
     } else {
         if (!parse::lex(trace, cin, true)) // Using the old version for now.
             return 1;
