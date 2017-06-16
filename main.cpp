54a497276cc66fc7becf418335715cbcd56196cf
Cerek Hillen <crockeo@gmail.com>
2017-06-16 13:09:16 -0400

    Letting you run through with a statically defined chunk of characters (for FuzzBALL).

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <sstream>
 
 #include "heap-layers/heaplayers.h"
 
@@ -11,20 +12,22 @@ using namespace heap;
 using namespace std;
 using namespace HL;
 
-int main()
+char program_input[4096];
+
+int main(int argc, char **argv)
 {
-#ifdef __AFL_LOOP
-    while (__AFL_LOOP(1000))
+    parse::trace_t trace;
+    if (argc == 2 && strcmp(argv[1], "fuzzmode") == 0)
     {
-#endif
-        parse::trace_t trace;
+        istringstream input_stream(program_input);
+        if (!parse::lex(trace, input_stream))
+            return 1;
+    } else {
         if (!parse::lex(trace, cin, true)) // Using the old version for now.
             return 1;
-
-        BrokenHeap<MallocHeap> brokenHeap;
-        if (!parse::execute<BrokenHeap<MallocHeap>>(brokenHeap, trace))
-        { }
-#ifdef __AFL_LOOP
     }
-#endif
+
+    BrokenHeap<MallocHeap> brokenHeap;
+    if (!parse::execute<BrokenHeap<MallocHeap>>(brokenHeap, trace))
+    { }
 }
